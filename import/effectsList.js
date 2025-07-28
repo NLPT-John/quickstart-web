0c731a8d988d063514fde8cc140245a416e92914
NLPT-John <160994540+NLPT-John@users.noreply.github.com>
2025-07-27 21:59:52 -0500

    Update effectsList.js

--- import/effectsList.js
+++ import/effectsList.js
@@ -1,5 +1,7 @@
 export const importedEffectsList = [
-  // Place your effect zip-file to import folder and add here zip-file name. Example:
+  // Phearlace your effect zip-file to import folder and add here zip-file name. Example:
   //'your_effect_1.zip',
   // 'your_effect_2.zip'
+  'heart-necklace.glb'
+  'gold-necklace.glb'
 ];
